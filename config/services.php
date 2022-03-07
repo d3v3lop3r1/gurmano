fe32ce3b3686180ead56e5d730d7f598db33fd33
d3v3lop3r1@gmail.com <d3v3lop3r1@gmail.com>
2022-03-07 09:01:55 -0800

    Some config changing

--- config/services.php
+++ config/services.php
@@ -29,12 +29,37 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
+
     'foxpost' => [
         'login' => env('FOXPOST_LOGIN'),
         'ecomcode' => env('FOXPOST_ECOM_CODE'),
         'ecompassw' => env('FOXPOST_ECOM_PASSWORD'),
         'apikey' => env('FOXPOST_API_KEY'),
     ],
+
+    'simplepay' => [
+        'apiendpoint' => env('API_ENDPOINT'),
+        'hufmerchant' => env('HUF_MERCHANT'),
+        'hufsecretkey' => env('HUF_SECRET_KEY'),          //secret key for account ID (HUF)
+        'eurmerchant' => env('EUR_MERCHANT'),            //merchant account ID (EUR)
+        'eursecretkey' => env('EUR_SECRET_KEY'),          //secret key for account ID (EUR)
+        'usdmerchant' => env('USD_MERCHANT'),            //merchant account ID (USD)
+        'usdsecretkey' => env('USD_SECRET_KEY'),          //secret key for account ID (USD)
+        'sandbox' => env('SANDBOX', true),
+        'urlback' => env('APP_URL', 'http://localhost') . '/simplepay/back',
+
+        
+        'urlsuccess' => env('APP_URL', 'http://localhost') . '/simplepay/success',       //url for successful payment
+        'urlfail' => env('APP_URL', 'http://localhost') . '/simplepay/fail',             //url for unsuccessful
+        'urlcancel' => env('APP_URL', 'http://localhost') . '/simplepay/cancel',         //url for cancell on payment page
+        'urltimeout' => env('APP_URL', 'http://localhost') . 'simplepay/timeout',       //url for payment page timeout
+        'getdata' => (isset($_GET['r']) && isset($_GET['s'])) ? ['r' => $_GET['r'], 's' => $_GET['s']] : [],
+        'postdata' => $_POST,
+        'serverdata' => $_SERVER,
+        'logger' => env('LOGGER', true),                              //basic transaction log
+        'logpath' => env('LOG_PATH', 'log'),                           //path of log file
+        'autochallenge' => env('AUTOCHALLENGE', true),  
+    ]
 
 
 ];
